a504142b28c89b7457fe54b91a8c8894cadad282
letrend <simon.trendel@tum.de>
2018-08-24 13:52:30 +0200

    rescue commit

--- mems_mics/synthesis/submodules/MEMS_THAT_SHIT.v
+++ mems_mics/synthesis/submodules/MEMS_THAT_SHIT.v
@@ -36,18 +36,18 @@ reg signed [31:0] y_1;
 reg signed [31:0] y_2;
 
 always @(posedge pdm_clk) begin
-	if (pdm) 
+	if (pdm)
 		raw_pdm_data <= 2147483647;
 	else
 		raw_pdm_data <= 0;
 end
 
-iir git_filter(
-	.clk(pdm_clk), 
-	.rst(reset), 
-	.x(raw_pdm_data), 
-	.y(filt_pdm_data_1)
-	);
+//iir git_filter(
+//	.clk(pdm_clk),
+//	.rst(reset),
+//	.x(raw_pdm_data),
+//	.y(filt_pdm_data_1)
+//	);
 
 always @(posedge clock, posedge reset) begin: AVALON_WRITE_ONCHIP_INTERFACE
 	if (reset == 1) begin
@@ -63,7 +63,7 @@ always @(posedge clock, posedge reset) begin: AVALON_WRITE_ONCHIP_INTERFACE
 		filt_pdm_data_7<= filt_pdm_data_6;
 		case(onchip_state)
 			IDLE: begin
-				if (dec_clk_prev == 0 &&  dec_clk == 1) begin 
+				if (dec_clk_prev == 0 &&  dec_clk == 1) begin
 					if (addr_cntr < 300) begin
 						addr_cntr <= 0; //addr_cntr + 1;
 						address <= addr_cntr;
@@ -75,7 +75,7 @@ always @(posedge clock, posedge reset) begin: AVALON_WRITE_ONCHIP_INTERFACE
 //						write_data[143:120] <=  filt_pdm_data_6;
 //						write_data[167:144] <=  filt_pdm_data_7;
 //						write_data[255:168] <= 0;
-						
+
 //						write_data[31:0] <= filt_pdm_data_1;
 //						write_data[63:32] <=  x_1;
 //						write_data[95:64] <=  x_2;
@@ -99,16 +99,16 @@ always @(posedge clock, posedge reset) begin: AVALON_WRITE_ONCHIP_INTERFACE
 					end
 				end
 			end
-			WAIT_FOR_TRANSMIT: begin		
+			WAIT_FOR_TRANSMIT: begin
 				if(waitrequest==0) begin
 					onchip_state <= IDLE;
-				end	
+				end
 			end
 			default: onchip_state <= IDLE;
 		endcase
 	end
 end
-	
+
 // ----- 32 bit range ----
 //	79749	159499	79749	1076132222	-2147483648	1071670424
 //79749	159499	79749	1080464368,00000	-2147483648,00000	1067338278,00000
@@ -117,8 +117,8 @@ end
 //79749	159499	79749	1090289387,00000	-2147483648,00000	1057513259,00000
 //79749	159499	79749	1091871563,00000	-2147483648,00000	1055931083,00000
 //18348550	18348550	0	2147483648,00000	-2110786547,00000	0
-	
-// ---- 24 bit range ----	
+
+// ---- 24 bit range ----
 //311	623	311	4203641	-8388608	4186212
 //311	623	311	4220563	-8388608	4169290
 //311	623	311	4235996	-8388608	4153857
@@ -138,130 +138,130 @@ end
 
 
 //621		1243	621	8388608	-16739949	8353827
-//filter #(
-//	.b0(621),
-//	.b1(1243),
-//	.b2(621), 
-//	.a0(8388608),
-//	.a1(-16739949),
-//	.a2(8353827)
-//	) layer_1 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(raw_pdm_data), 
-//	.y(filt_pdm_data_1)
-//	);
+filter #(
+	.b0(621),
+	.b1(1243),
+	.b2(621),
+	.a0(8388608),
+	.a1(-16739949),
+	.a2(8353827)
+	) layer_1 (
+	.clk(pdm_clk),
+	.reset(reset),
+	.x(raw_pdm_data),
+	.y(filt_pdm_data_1)
+	);
 //
 ////619		1238	619	8388608	-16672830	8286698
 //filter #(
 //	.b0(621),
 //	.b1(1238),
-//	.b2(619), 
+//	.b2(619),
 //	.a0(8388608),
 //	.a1(-16672830),
 //	.a2(8286698)
 //	) layer_2 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_1), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_1),
 //	.y(filt_pdm_data_2),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
-	
+
 //616		1233	616	8388608	-16612088	8225947
 //filter #(
 //	.b0(616),
 //	.b1(1233),
-//	.b2(616), 
+//	.b2(616),
 //	.a0(8388608),
 //	.a1(-16612088),
 //	.a2(8225947)
 //	) layer_3 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_2), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_2),
 //	.y(filt_pdm_data_3),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
-//	
+//
 ////615		1230	615	8388608	-16561084	8174936
 //filter #(
 //	.b0(615),
 //	.b1(1230),
-//	.b2(615), 
+//	.b2(615),
 //	.a0(8388608),
 //	.a1(-16561084),
 //	.a2(8174936)
 //	) layer_4 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_3), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_3),
 //	.y(filt_pdm_data_4),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
-//	
+//
 ////613		1227	613	8388608	-16522585	8136430
 //filter #(
 //	.b0(613),
 //	.b1(1227),
-//	.b2(613), 
+//	.b2(613),
 //	.a0(8388608),
 //	.a1(-16522585),
 //	.a2(8136430)
 //	) layer_5 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_4), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_4),
 //	.y(filt_pdm_data_5),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
-//	
+//
 ////612		1225	612	8388608	-16498643	8112485
 //filter #(
 //	.b0(612),
 //	.b1(1225),
-//	.b2(612), 
+//	.b2(612),
 //	.a0(8388608),
 //	.a1(-16498643),
 //	.a2(8112485)
 //	) layer_6 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_5), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_5),
 //	.y(filt_pdm_data_6),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
-//	
+//
 ////71674	71674	0		8388608	-8245260		0
 //filter #(
 //	.b0(71674),
 //	.b1(71674),
-//	.b2(0), 
+//	.b2(0),
 //	.a0(8388608),
 //	.a1(-8245260),
 //	.a2(0)
 //	) layer_7 (
-//	.clk(pdm_clk), 
-//	.reset(reset), 
-//	.x(filt_pdm_data_6), 
+//	.clk(pdm_clk),
+//	.reset(reset),
+//	.x(filt_pdm_data_6),
 //	.y(filt_pdm_data_7),
 //	.x_1_out(x_1),
-//	.x_2_out(x_2), 
+//	.x_2_out(x_2),
 //	.y_1_out(y_1),
 //	.y_2_out(y_2)
 //	);
@@ -291,7 +291,7 @@ endmodule
 //		filt_pdm_data_5 <= 49'd0;
 //		filt_pdm_data_6 <= 49'd0;
 //		filt_pdm_data_7 <= 49'd0;
-//	end 
+//	end
 //end
 
 	// not so smart array filter decleration
@@ -302,7 +302,7 @@ endmodule
 //	b0[4] <= 32'd79749;
 //	b0[5] <= 32'd79749;
 //	b0[6] <= 32'd18348550;
-//	
+//
 //	b1[0] <= 32'd159499;
 //	b1[1] <= 32'd159499;
 //	b1[2] <= 32'd159499;
@@ -310,7 +310,7 @@ endmodule
 //	b1[4] <= 32'd159499;
 //	b1[5] <= 32'd159499;
 //	b1[6] <= 32'd18348550;
-//	
+//
 //	b2[0] <= 32'd79749;
 //	b2[1] <= 32'd79749;
 //	b2[2] <= 32'd79749;
@@ -342,80 +342,80 @@ endmodule
 //	a2[4] <= 48'd105751325900000;
 //	a2[5] <= 48'd105593108300000;
 //	a2[6] <= 48'd0;
-//	
+//
 
 ////		if(slow_clk_prev == 0 && pdm_clk == 1)begin // rising edge jof pdm_clk
 //			case(pdm_state)
-//			
+//
 //			default: begin
 //				pdm_state <= DATA_TO_REG;
 //			end
 //			endcase
 ////			end
-				
+
 //		case(onchip_state)
 //			IDLE: begin
-////				if(slow_clk_prev == 0 && pdm_clk == 1)begin 
+////				if(slow_clk_prev == 0 && pdm_clk == 1)begin
 ////						onchip_state <= DATA_TO_REG;
 ////					end
 //			end
-			
+
 //			DATA_TO_REG: begin
 //				raw_pdm_data[2] <= raw_pdm_data[1];
 //				raw_pdm_data[1] <= raw_pdm_data[0];
 //				raw_pdm_data[0] <= pdm * 8'hff;
 //				onchip_state <= FILTER;
 //			end
-			
+
 //			FILTER: begin
 //			// row 1
 //				filt_pdm_data_1[2] = filt_pdm_data_1[1];
 //				filt_pdm_data_1[1] = filt_pdm_data_1[0];
 //				filt_pdm_data_1[0] = (b0[0] * raw_pdm_data[0] + b1[0] * raw_pdm_data[1] + b2[0] * raw_pdm_data[2]
 //										- a1[0] * filt_pdm_data_1[1] - a2[0] * filt_pdm_data_1[2]) / a0[0];
-//				
-//			
+//
+//
 //				// row 2
 //				filt_pdm_data_1[2] = filt_pdm_data_2[1];
 //				filt_pdm_data_2[1] = filt_pdm_data_2[0];
 //				filt_pdm_data_2[0] = (b0[1] * filt_pdm_data_1[0] + b1[1] * filt_pdm_data_1[1] + b2[1] * filt_pdm_data_1[2]
 //										- a1[1] * filt_pdm_data_2[1] - a2[1] * filt_pdm_data_2[2]) / a0[1];
-//				
-//				
-//			
+//
+//
+//
 //				// row 3
 //				filt_pdm_data_1[2] = filt_pdm_data_3[1];
 //				filt_pdm_data_3[1] = filt_pdm_data_3[0];
 //				filt_pdm_data_3[0] = (b0[4] * filt_pdm_data_2[0] + b1[2] * filt_pdm_data_2[1] + b2[2] * filt_pdm_data_2[2]
 //										- a1[2] * filt_pdm_data_3[1] - a2[2] * filt_pdm_data_3[2]) / a0[2];
-//										
-//				
-//			
+//
+//
+//
 //				// row 4
 //				filt_pdm_data_1[2] = filt_pdm_data_4[1];
 //				filt_pdm_data_4[1] = filt_pdm_data_4[0];
 //				filt_pdm_data_4[0] = (b0[3] * filt_pdm_data_3[0] + b1[3] * filt_pdm_data_3[1] + b2[3] * filt_pdm_data_3[2]
 //										- a1[3] * filt_pdm_data_4[1] - a2[3] * filt_pdm_data_4[2]) / a0[3];
-//										
-//										
-//				
-//			
+//
+//
+//
+//
 //				// row 5
 //				filt_pdm_data_1[2] = filt_pdm_data_5[1];
 //				filt_pdm_data_5[1] = filt_pdm_data_5[0];
 //				filt_pdm_data_5[0] = (b0[4] * filt_pdm_data_4[0] + b1[4] * filt_pdm_data_4[1] + b2[4] * filt_pdm_data_4[2]
 //										- a1[4] * filt_pdm_data_5[1] - a2[4] * filt_pdm_data_5[2]) / a0[4];
-//										
-//				
-//			
+//
+//
+//
 //				// row 6
 //				filt_pdm_data_1[2] = filt_pdm_data_1[1];
 //				filt_pdm_data_1[1] = filt_pdm_data_1[0];
 //				filt_pdm_data_6[0] = (b0[5] * filt_pdm_data_5[0] + b1[5] * filt_pdm_data_5[1] + b2[5] * filt_pdm_data_5[2]
 //										- a1[5] * filt_pdm_data_6[1] - a2[5] * filt_pdm_data_6[2]) / a0[5];
-//										
-//				
-//			
+//
+//
+//
 //				// row 7
 //				filt_pdm_data_1[2] = filt_pdm_data_7[1];
 //				filt_pdm_data_7[1] = filt_pdm_data_7[0];
@@ -423,7 +423,7 @@ endmodule
 //										- a1[6] * filt_pdm_data_7[1] - a2[6] * filt_pdm_data_7[2]) / a0[6];
 //				onchip_state = DECIMATION;
 //			end
-			
+
 //			DECIMATION: begin
 //				if (dec_cnt == 0) begin
 //					write_data <= filt_pdm_data_7[0];
@@ -438,7 +438,7 @@ endmodule
 //					onchip_state <= IDLE;
 //				end
 //			end
-//       ---- old version ----		
+//       ---- old version ----
 //			SUM_BITS: begin
 //			// low pass filter summation
 ////				pdm_sum = 0;
@@ -454,7 +454,7 @@ endmodule
 
 //reg [31:0] b0 [6:0];
 //
-//reg [31:0] b1 [6:0]; 
+//reg [31:0] b1 [6:0];
 //reg [31:0] b2 [6:0];
 //reg [47:0] a0 [6:0];
 //reg [47:0] a1 [6:0];
@@ -469,4 +469,4 @@ endmodule
 //		write_data <= 8'hc5;//filt_pdm_data_7[7:0];
 ////		onchip_state <= TRANSMIT;
 //	end
-//end 
+//end
